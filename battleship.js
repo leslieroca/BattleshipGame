3ec6117423c13f9553ad073fc59124cda6d408a3
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2019-10-09 16:10:01 -0700

    Testing the view displaying Miss, Hit and Message

---
+++ battleship.js
@@ -0,0 +1,24 @@
+//testing the view
+var view = {
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTLM = msg;
+  },
+  displayHit: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  }
+};
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("Tap tap, is this thing on?");
