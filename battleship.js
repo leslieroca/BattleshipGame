9827df5180aa4e21800e9c169f8becdc809431f4
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2019-10-09 17:01:56 -0700

    Model object and fire method implementation

--- battleship.js
+++ battleship.js
@@ -1,4 +1,3 @@
-//testing the view
 var view = {
   displayMessage: function(msg) {
     var messageArea = document.getElementById("messageArea");
@@ -14,11 +13,39 @@ var view = {
   }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
+//testing the view:
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
 
-view.displayMessage("Tap tap, is this thing on?");
+// view.displayMessage("Tap tap, is this thing on?");
+///////////////////////////////////////////////////////////////////////////////////
+
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+	//hardcode the ships initial locations for references
+	ships: [
+		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] }
+  ],
+  fire: function (guess) {
+
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var locations = ship.locations;
+      var index = locations.indexOf(guess);
+      if(index >= 0) {
+        ships.hits[index] = "hit";
+        return true;
+      }
+    }
+    return false;
+  }
+};
