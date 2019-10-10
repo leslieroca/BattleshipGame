f7e90ee7928d7398972b0eb8b35a44f6b15c4a11
Leslie Rodriguez Cabrera <leslie.roca56@gmail.com>
2019-10-09 17:33:54 -0700

    Implement isSunk method, update shipsSunk property state and notify the view object of hit or miss

--- battleship.js
+++ battleship.js
@@ -29,23 +29,37 @@ var model = {
 	numShips: 3,
 	shipLength: 3,
 	shipsSunk: 0,
-	//hardcode the ships initial locations for references
+	//Hardcode the ships initial locations for references.
 	ships: [
 		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
 		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
     { locations: ["10", "11", "12"], hits: ["", "", ""] }
   ],
   fire: function (guess) {
-
     for (var i = 0; i < this.numShips; i++) {
       var ship = this.ships[i];
-      var locations = ship.locations;
-      var index = locations.indexOf(guess);
+      var index = ship.locations.indexOf(guess);
       if(index >= 0) {
         ships.hits[index] = "hit";
+        view.displayHit(guess);   //Notify the view that we got a hit at the location in guess.
+        view.displayMessage("HIT!");   //Ask the view to display the message "HIT!".
+        if(this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!") //Let the player know this hit sank the battleship.
+          this.shipsSunk++;
+        }
         return true;
       }
     }
+    view.displayMiss(guess); //Notify the view that we got a miss at the location in guess.
+    view.displayMessage("You missed."); //Ask the vie to display "You missed.".
     return false;
+  },
+  isSunk: function(ship) {
+    for (var i = 0; i < hists.length; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false
+      }
+    }
+    return true;
   }
 };
